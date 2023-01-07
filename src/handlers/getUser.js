699d471ffc87f18610eddff7dbb99d8f20e97215
bibhasingh <bibhaanil@gmail.com>
2023-01-07 17:46:19 +0100

    api call

--- src/handlers/getUser.js
+++ src/handlers/getUser.js
@@ -5,6 +5,10 @@ const getUser = function () {
   console.log("Getting ID");
   const id = Number(document.querySelector(".text").value);
 
+  while (document.body.childElementCount > 1) {
+    document.body.removeChild(document.body.lastChild);
+  }
+
   userDetails(id)
     .then((user) => userDisplay(user))
     .catch((err) => console.error(err));
